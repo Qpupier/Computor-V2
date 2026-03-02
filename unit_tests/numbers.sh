b6842d21e452555272fb4ecd3a283890cb415a90
Quentin PUPIER <qpupier@student.42lyon.fr>
2026-03-02 22:08:10 +0100

    CI - Unit tests - Improve display errors

--- unit_tests/numbers.sh
+++ unit_tests/numbers.sh
@@ -11,5 +11,3 @@ run_test "2+1" "3"
 run_test "0*1" "0"
 run_error "1/0" "Division by zero is impossible"
 run_error "1 / (3 * 4 + (-12))" "Division by zero is impossible"
-
-run_error "i[[42]]" "Matrix and imaginary unit cannot multiply each other"
--- unit_tests/utils.sh
+++ unit_tests/utils.sh
@@ -1,42 +1,51 @@
 #!/bin/sh
 
+delete_files()
+{
+	rm -f out err expected
+}
+
 run()
 {
-	echo "Testing: \"$1\""
+	echo "\nTesting: \"$1\""
 	printf '%s\n' "$1" | ./computor-v2 > out 2> err
+	tail -c +3 out > out.tmp && mv out.tmp out && rm -f out.tmp
+	truncate -s -4 out
+	truncate -s -1 err
 	sed -i 's/\x1b\[[0-9;]*m//g' out err
 }
 
 print_error()
 {
-	echo "Standard output:"
+	echo -n "\nStandard output:	["
 	cat out
-	echo "Error output:"
+	echo -n "]\nError output:		["
 	cat err
-	rm -f expected
+	echo "]"
+	delete_files
 	exit 1
 }
 
 run_test()
 {
-	run $1
-	printf '> %s\n> \n' "$2" > expected
+	run "$1"
+	printf '%s' "$2" > expected
 	if ! diff -u expected out > /dev/null; then
 		echo "Output differs from expected:"
-		diff -u expected out
+		diff -u expected out | grep -v "\ No newline at end of file"
 		print_error
 	fi
-	rm -f expected
+	delete_files
 }
 
 run_error()
 {
-	run $1
-	printf '%s\n' "$2" > expected
+	run "$1"
+	printf '%s' "$2" > expected
 	if ! diff -u expected err >/dev/null; then
 		echo "Error output differs from expected:"
-		diff -u expected err
+		diff -u expected err | grep -v "\ No newline at end of file"
 		print_error
 	fi
-	rm -f expected
+	delete_files
 }
