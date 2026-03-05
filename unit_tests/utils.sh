e9213cf4eec7fc9558c43c0d91a91933bb4327df
Quentin PUPIER <qpupier@student.42lyon.fr>
2026-03-05 11:32:50 +0100

    CI - Improve UI to adjust for matrices

--- unit_tests/utils.sh
+++ unit_tests/utils.sh
@@ -2,37 +2,42 @@
 
 delete_files()
 {
-	rm -f out err expected
+	rm -f output error expected
 }
 
 run()
 {
-	echo -n "\033[35;3mTesting:\033[0m \"$1\" => \"$2\" "
-	echo "$1" | MAKEFLAGS=--no-print-directory make run > out 2> err
-	truncate -s -1 out err
-	sed -i 's/\x1b\[[0-9;]*m//g' out err
+	echo -n "\033[35;3mTesting:\033[0m \"$1\"\n$2 "
+	echo "$1" | MAKEFLAGS=--no-print-directory make run > output 2> error
+	truncate -s -1 output error
+	sed -i 's/\x1b\[[0-9;]*m//g' output error
 }
 
 print_error()
 {
-	echo -n "\nStandard output:	\033[30m[\033[0m"
-	cat out
-	echo -n "\033[30m]\033[0m\nError output:		["
-	cat err
-	echo "]"
+	echo "\033[30mStandard output:\033[0m"
+	cat output
+	if [ -s output ]; then
+		echo
+	fi
+	echo "\033[30mError output:\033[0m"
+	cat error
+	if [ -s error ]; then
+		echo
+	fi
 	delete_files
 	exit 1
 }
 
 test_leaks_and_errors()
 {
-	echo "$1" | MAKEFLAGS=--no-print-directory make valgrind > out 2> err
-	if ! grep -q "All heap blocks were freed -- no leaks are possible" err || ! grep -q "ERROR SUMMARY: 0 errors from 0 contexts (suppressed: 0 from 0)" err; then
+	echo "$1" | MAKEFLAGS=--no-print-directory make valgrind > output 2> error
+	if ! grep -q "All heap blocks were freed -- no leaks are possible" error || ! grep -q "ERROR SUMMARY: 0 errors from 0 contexts (suppressed: 0 from 0)" error; then
 		echo "❌"
 		if [ "$2" != "debug" ]; then
 			return 1
 		fi
-		cat err
+		cat error
 		delete_files
 		exit 1
 	fi
@@ -45,13 +50,15 @@ run_test()
 {
 	run "$1" "$2" "$3"
 	printf '%s' "$2" > expected
-	if ! diff -u expected out > /dev/null; then
+	if ! diff -u expected output > /dev/null; then
 		echo "❌"
 		if [ "$3" != "debug" ]; then
 			return 1
 		fi
-		echo "Output differs from expected:"
-		diff -u expected out | grep -v "\ No newline at end of file"
+		echo "\n\033[30mOutput differs from expected:\033[0m"
+		echo -n "\033[31m"
+		diff -u expected output | grep -v "\ No newline at end of file"
+		echo "\033[0m"
 		print_error
 	fi
 	test_leaks_and_errors "$1" "$3"
@@ -61,13 +68,15 @@ run_error()
 {
 	run "$1" "$2" "$3"
 	printf '%s' "$2" > expected
-	if ! diff -u expected err > /dev/null; then
+	if ! diff -u expected error > /dev/null; then
 		echo "❌"
 		if [ "$3" != "debug" ]; then
 			return 1
 		fi
-		echo "Error output differs from expected:"
-		diff -u expected err | grep -v "\ No newline at end of file"
+		echo "\n\033[30mError output differs from expected:\033[0m"
+		echo -n "\033[31m"
+		diff -u expected error | grep -v "\ No newline at end of file"
+		echo "\033[0m"
 		print_error
 	fi
 	test_leaks_and_errors "$1" "$3"
