7ad0eb0a08e0ba38a3c785a0b1c06d1fe327c1cd
Quentin PUPIER <qpupier@student.42lyon.fr>
2026-05-05 15:26:14 +0200

    [test] Adapt polynomials print

--- unit_tests/polynomials.sh
+++ unit_tests/polynomials.sh
@@ -10,12 +10,12 @@ run_test "2 * varA - 5 %4" "2varA - 1" $1 ; status=$((status | $?))
 run_test "4 -5 + (x + 2)^2 - 4" "x^2 + 4x - 1" $1 ; status=$((status | $?))
 run_test "4x + 5 - 2" "4x + 3" $1 ; status=$((status | $?))
 run_test "2 * (4 + varA + 3)" "2varA + 14" $1 ; status=$((status | $?))
-run_test "2 *(2 + 4 *varC -4 /3)" "8varC + 4/3" $1 ; status=$((status | $?))
+run_test "2 *(2 + 4 *varC -4 /3)" "(24varC + 4) / 3" $1 ; status=$((status | $?))
 run_test "2*b+b" "3b" $1 ; status=$((status | $?))
-run_test "2* y + 4 -2 * 4+1/3" "2y - 11/3" $1 ; status=$((status | $?))
+run_test "2* y + 4 -2 * 4+1/3" "(6y - 11) / 3" $1 ; status=$((status | $?))
 run_test "-x^2 +i * x" "-x^2 + i * x" $1 ; status=$((status | $?))
 run_test "-1 * x^2 + i*x -3x +1" "-x^2 + (i - 3)x + 1" $1 ; status=$((status | $?))
 
-run_error "43 * y / (4 % 2 * y)" "Unsupported: Division not supported between polynomials with these powers" $1 ; status=$((status | $?))
+run_error "43 * y / (4 % 2 * y)" "Division by zero is impossible" $1 ; status=$((status | $?))
 
 return $status
