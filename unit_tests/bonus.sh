9d870d89354b841c86d6e601df90c22e79895e02
Quentin PUPIER <qpupier@student.42lyon.fr>
2026-06-19 11:28:32 +0200

    [test] (Bonus) Vectors

--- unit_tests/bonus.sh
+++ unit_tests/bonus.sh
@@ -19,4 +19,16 @@ run_test "~[[1, 0, 2, 0];[1, 1, 0, 0];[1, 2, 1, 1];[0, 0, 1, 1]]" "[ 0 , 2 , -1 
 
 run_error "~42" "Matrix inversion is only possible for square matrices" $1 ; status=$((status | $?))
 
+echo "\n\033[34;1mTesting vectors\033[0m\n"
+
+run_test "[1, 2, 3]" "[1, 2, 3]" $1 ; status=$((status | $?))
+run_test "[1, 2, 3] + [4, 5, 6]" "[5, 7, 9]" $1 ; status=$((status | $?))
+run_test "[1, 2, 3][[4];[5];[6]]" "[ 32 ]" $1 ; status=$((status | $?))
+run_test "[1, 2, 3] ** [[4];[5];[6]]" "[ 32 ]" $1 ; status=$((status | $?))
+run_test "[[4];[5];[6]] ** [1, 2, 3]" "[ 4 , 8 , 12 ]
+[ 5 , 10 , 15 ]
+[ 6 , 12 , 18 ]" $1 ; status=$((status | $?))
+
+run_error "[1, 2, 3] * [[4];[5];[6]]" "Matrix operation: incompatible dimensions" $1 ; status=$((status | $?))
+
 return $status
