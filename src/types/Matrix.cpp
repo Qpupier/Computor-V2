df3155c66d3871f0c65bd1bfadd35ff51ef5ab75
Quentin PUPIER <qpupier@student.42lyon.fr>
2026-08-04 14:19:08 +0200

    [feat] (Bonus) Cosine function for Matrices

--- src/types/Matrix.cpp
+++ src/types/Matrix.cpp
@@ -6,7 +6,7 @@
 /*   By: qpupier <qpupier@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/04 17:07:55 by qpupier           #+#    #+#             */
-/*   Updated: 2026/08/04 14:03:04 by qpupier          ###   ########lyon.fr   */
+/*   Updated: 2026/08/04 14:12:54 by qpupier          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -307,6 +307,26 @@ static Matrix*	exp_get_new_term(const Matrix& matrix, long long int k)
 	new_term = *power / *factorial;
 	delete power;
 	delete factorial;
+	return (new_term);
+}
+
+static Matrix*	cos_get_new_term(const Matrix& matrix, long long int k)
+{
+	Matrix*		matrix_power;
+	Matrix*		new_term;
+	Rational*	coeff;
+	Rational*	coeff_num;
+	Rational*	factorial;
+
+	coeff_num = Rational(-1) ^ k;
+	factorial = Rational(2 * k).factorial();// [ ] Verifier overflow
+	coeff = *coeff_num / *factorial;
+	matrix_power = matrix ^ (2 * k);
+	delete coeff_num;
+	delete factorial;
+	new_term = *coeff * *matrix_power;
+	delete coeff;
+	delete matrix_power;
 	return (new_term);
 }
 
@@ -1275,8 +1295,30 @@ IType*			Matrix::clone(void) const
 
 IType*			Matrix::cos(void) const// TODO
 {
-	throw ERROR_UNEXPECTED;
-	return (nullptr);
+	long long int	k(0);
+	Matrix*			result;
+	Matrix*			tmp;
+	Matrix*			new_term;
+
+	result = Matrix(this->_width, this->_height) * 0;
+	while (true)
+	{
+		new_term = cos_get_new_term(*this, k++);
+		if (!*new_term)
+		{
+			delete new_term;
+			return (result);
+		}
+		tmp = result;
+		result = *result + *new_term;
+		delete new_term;
+		if (is_round_equal(*result, *tmp))
+			break;
+		delete tmp;
+	}
+	delete tmp;
+	result->round();
+	return (result);
 }
 
 IType*			Matrix::exp(void) const
