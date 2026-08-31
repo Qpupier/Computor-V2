fd470307c7fb80b748a316d0b82650f41736d65e
Quentin PUPIER <qpupier@student.42lyon.fr>
2026-08-31 17:53:32 +0200

    [test] Speed up valgrind in CI

--- unit_tests/utils.sh
+++ unit_tests/utils.sh
@@ -37,7 +37,7 @@ print_error()
 
 test_leaks_and_errors()
 {
-	echo "$1" | valgrind --leak-check=full --track-origins=yes --show-leak-kinds=all -s ./computor-v2 > output 2> error
+	echo "$1" | valgrind --leak-check=full ./computor-v2 > output 2> error
 	if ! grep -q "All heap blocks were freed -- no leaks are possible" error || ! grep -q "ERROR SUMMARY: 0 errors from 0 contexts (suppressed: 0 from 0)" error; then
 		echo "❌"
 		if [ "$2" != "debug" ]; then
