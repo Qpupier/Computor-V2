9a257c1bf12b8a400d1838ebe2bb22be9654110c
Quentin PUPIER <qpupier@student.42lyon.fr>
2026-09-02 17:22:42 +0200

    [test] Adapt test in format.sh

--- unit_tests/format.sh
+++ unit_tests/format.sh
@@ -13,6 +13,6 @@ run_error "42 = ??" "Too many '?' in the expression" $1 ; status=$((status | $?)
 run_error "()" "Brackets (parentheses, absolute values or norms) cannot be empty" $1 ; status=$((status | $?))
 run_error "42()" "Brackets (parentheses, absolute values or norms) cannot be empty" $1 ; status=$((status | $?))
 run_error "*1" "Operator cannot be at the beginning or end of an expression" $1 ; status=$((status | $?))
-run_error "1++1" "Two operators cannot be adjacent" $1 ; status=$((status | $?))
+run_error "1*/1" "Two operators cannot be adjacent" $1 ; status=$((status | $?))
 
 return $status
